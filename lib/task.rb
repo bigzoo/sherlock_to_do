7addf4d08e51fd2b2322106e7ddfc305423cb5a9
Chris Nyaga <nyagachris75@gmail.com>
2017-07-03 11:27:21 +0300

    This commit adds a class method to return all tasks and class variable to hold all the tasks

--- lib/task.rb
+++ lib/task.rb
@@ -1,10 +1,16 @@
 class Task
+    @@all_tasks = []
+
     define_method(:initialize) do |description|
       @description = description
     end
 
     define_method(:description) do
       @description
+    end
+
+    define_singleton_method(:all) do
+      @@all_tasks
     end
   end
   
